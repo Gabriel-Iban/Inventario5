22c646df126f2f6886c3bfae7758b965ac762c4b
Gabriel Ibán <gabriel.iban.cf@gmail.com>
2020-12-13 10:26:01 +0100

    Antes de cambio a seleccionar workstation en monitor

--- Inventario5/Models/Monitor.cs
+++ Inventario5/Models/Monitor.cs
@@ -8,7 +8,7 @@ namespace Inventario5.Models
     public partial class Monitor
     {
         public int Id { get; set; }
-        public byte? TamanoPantalla { get; set; }
+        public float? TamanoPantalla { get; set; }
         public int? ResolucionX { get; set; }
         public int? ResolucionY { get; set; }
         public int? IdWorkstation { get; set; }
